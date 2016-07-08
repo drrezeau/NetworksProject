a2e5b6ed192696c3690f55a0449d1f53535d36bb
MoonRock28 <ste11073@byui.edu>
2016-07-08 12:58:08 -0600

    added peer2 for testing

---
+++ webgl/js/peer2.js
@@ -0,0 +1,36 @@
+var myUserName;
+var peerUserName;
+
+function connect2Server() {
+    myUserName = $("#myUsername").value;
+
+
+    var peer = new Peer(MyUserName, {key: 'mm63227qa567ds4i'})
+    peer.on('open', function (id) {
+        console.log('My peer ID is: ' + id);
+    });
+
+    document.getElementById("getMyUsername").display = "none";
+    document.getElementById("getPeerUsername").display = "block";
+
+}
+
+function connect2Peer() {
+    peerUserName = $("#peerUsername").value;
+
+    var conn = peer.connect(peerUserName);
+
+    conn.on('open', function(){
+        conn.send('hi!');
+    });
+
+    document.getElementsById("getPeerUsername").display = "none";
+}
+
+function receivePeer() {
+    peer.on('connection', function(conn) {
+        conn.on('data', function(data){
+            console.log(data);
+            });
+    });
+}
